4dc30fd4817f5c72611fc776f597bfbfc682ad4a
Jose <josebalserospinto@gmail.com>
2023-12-07 18:07:04 -0500

    Import of sept. 2023 project for further refactorization and refinement

---
+++ application/client/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Main () {
+    return (
+        <div>
+            IntrepidFrog, banking application for seamless transaction history and secure payments.
+        </div>
+    )
+}
