b4ffbd7ba6b1c78373f0238c3467fb4608e6a04a
Jose <josebalserospinto@gmail.com>
2023-12-08 21:43:05 -0500

    Configuration of CSS modules

--- application/client/pages/index.tsx
+++ application/client/pages/index.tsx
@@ -1,7 +1,19 @@
+import Head from "next/head";
+
 export default function Main () {
     return (
-        <div>
+        <div >
+            <Head>
+                <meta lang='es' charSet="unicode"/>
+                <meta name="autho" content="Banking application for easy transaction tracking, user management, and secure money transfers." />
+                <title>IntrepidFrog</title>
+                <link rel='icon' href="/icon-frog.jpg"></link>
+            </Head>
             IntrepidFrog, banking application for seamless transaction history and secure payments.
+
+            <section>
+
+            </section>
         </div>
     )
 }
