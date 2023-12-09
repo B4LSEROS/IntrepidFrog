140e4a820e5659c0069751c59cdd4589f58e5f35
Jose <josebalserospinto@gmail.com>
2023-12-09 13:29:47 -0500

    CSS advance styling, keyframes, and sass

--- application/client/components/Banner.tsx
+++ application/client/components/Banner.tsx
@@ -4,6 +4,13 @@ import css from '../css/banner.module.css';
 export default function Banner () {
     return (
         <section className={css.main}>
+            <h1>Intrepid Frog</h1>
+
+            <div>
+                <h2>Transaction History</h2>
+                <h2>User Authentication</h2>
+                <h2>Secure Transfers</h2>
+            </div>
             
 
         </section>
--- application/client/pages/index.tsx
+++ application/client/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Image from 'next/image';
 import Banner from "../components/Banner";
+import logo from '../public/logo.png';
 import css from '../css/main.module.css';
 
 export default function Main () {
@@ -7,14 +9,19 @@ export default function Main () {
         <div className={css.main}>
             <Head>
                 <meta lang='es' charSet="unicode"/>
-                <meta name="autho" content="Banking application for easy transaction tracking, user management, and secure money transfers." />
+                <meta name="description" content="Banking application for easy transaction tracking, user management, and secure money transfers." />
                 <title>IntrepidFrog</title>
                 <link rel='icon' href="/icon-frog.jpg"></link>
             </Head>
 
             <Banner />
-            <section className={css.loginContainer}>
+            <section className={css.section2}>
+                <form className={css.loginContainer}>
+                    
+                        <Image className={css.logo}alt='IntrepidFrog Logo'src={logo}></Image>
+                        <h1>Login</h1>
 
+                </form>
             </section>
         </div>
     )
