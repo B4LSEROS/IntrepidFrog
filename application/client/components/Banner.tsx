60ff6baa2c669fac238227531074f4d9a93c5ad5
Jose <josebalserospinto@gmail.com>
2023-12-09 08:21:32 -0500

    Further configuration of CSS modules and initial setup of layout skeleton

---
+++ application/client/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from '../css/banner.module.css';
+
+export default function Banner () {
+    return (
+        <section className={css.main}>
+            
+
+        </section>
+    )
+}
--- application/client/pages/index.tsx
+++ application/client/pages/index.tsx
@@ -1,17 +1,19 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
+import css from '../css/main.module.css';
 
 export default function Main () {
     return (
-        <div >
+        <div className={css.main}>
             <Head>
                 <meta lang='es' charSet="unicode"/>
                 <meta name="autho" content="Banking application for easy transaction tracking, user management, and secure money transfers." />
                 <title>IntrepidFrog</title>
                 <link rel='icon' href="/icon-frog.jpg"></link>
             </Head>
-            IntrepidFrog, banking application for seamless transaction history and secure payments.
 
-            <section>
+            <Banner />
+            <section className={css.loginContainer}>
 
             </section>
         </div>
